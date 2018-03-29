ee2b930fa21e6fc987b5fd6a16837a46ef736841
neo <wjqaifilm@126.com>
2018-03-29 23:55:02 +0800

    test

--- imooc-security-demo/src/main/java/com/imooc/security/MyUserDetailsService.java
+++ imooc-security-demo/src/main/java/com/imooc/security/MyUserDetailsService.java
@@ -1,15 +1,16 @@
-package com.imooc.security.browser;
+package com.imooc.security;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.social.security.SocialUser;
+import org.springframework.social.security.SocialUserDetails;
+import org.springframework.social.security.SocialUserDetailsService;
 import org.springframework.stereotype.Component;
 
 /**
@@ -19,7 +20,7 @@ import org.springframework.stereotype.Component;
  * @create: 2018-03-12 21:07
  **/
 @Component
-public class MyUserDetailsService implements UserDetailsService {
+public class MyUserDetailsService implements UserDetailsService,SocialUserDetailsService {
     private Logger logger = LoggerFactory.getLogger(getClass());
     //调用他的encode方法每此生成的密码加密字符串都不同，即使同一密码两次加密的结果也不一样（实现类BCryptPasswordEncoder）
     @Autowired
@@ -27,13 +28,26 @@ public class MyUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        logger.info("登录用户名：" + username);
+        logger.info("表单登录用户名：" + username);
+        UserDetails userDetails = buildUser(username);
+        return userDetails;
+    }
+
+
+    @Override
+    public SocialUserDetails loadUserByUserId(String userId) throws UsernameNotFoundException {
+        logger.info("社交登录用户id：" + userId);
+        SocialUserDetails socialUserDetails = buildUser(userId);
+        return socialUserDetails;
+    }
+
+    private SocialUserDetails buildUser(String userId){
         //return new User(username,"123456", AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
         //四个boolean代表UserDetails四个方法中关于用户和密码的四种状态
         String password = passwordEncoder.encode("123456");
         logger.info(password);
-        return new User(username, password, true,
-        true, true,
-        true, AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+        return new SocialUser(userId, password, true,
+                true, true,
+                true, AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
     }
 }
